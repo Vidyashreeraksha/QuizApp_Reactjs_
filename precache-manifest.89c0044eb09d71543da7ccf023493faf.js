31f7b55a334ae1433a901de2d167b7db5ae42063
vidya shree raksha <vidyabharadwaj7@gmail.com>
2020-10-06 20:50:20 +0530

    Updates

---
+++ precache-manifest.89c0044eb09d71543da7ccf023493faf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc0986ef9bea2ac0923a3d988edc4f65",
+    "url": "/QuizApp_Reactjs_/index.html"
+  },
+  {
+    "revision": "b101642c4dc182d60c41",
+    "url": "/QuizApp_Reactjs_/static/css/main.0f93a89c.chunk.css"
+  },
+  {
+    "revision": "a7030578b9f477d2d465",
+    "url": "/QuizApp_Reactjs_/static/js/2.12bcec45.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/QuizApp_Reactjs_/static/js/2.12bcec45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b101642c4dc182d60c41",
+    "url": "/QuizApp_Reactjs_/static/js/main.64dedd08.chunk.js"
+  },
+  {
+    "revision": "4fe68efc80c18d936ee5",
+    "url": "/QuizApp_Reactjs_/static/js/runtime-main.019d703a.js"
+  }
+]);
